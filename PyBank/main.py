442185a1cee83006a3c598abadabf2bbe43ab8af
zovel <zoe.a.velie@gmail.com>
2018-09-05 12:18:13 -0500

    total months code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,15 +3,13 @@ import os
 import csv
 budget_data=os.path.join('budget_data.csv')
 net=0
-total=0
 
 #The total number of months included in the dataset
 with open(budget_data,newline="") as totalmonths_csv:
     totalmonths_reader=csv.reader(totalmonths_csv,delimiter=",")
     next(totalmonths_reader,None)
-    for row in totalmonths_reader:
-        total+=int(row[0])
-        
+    value=len(list(totalmonths_reader))
+
 #The total net amount of "Profit/Losses" over the entire period
 with open (budget_data,newline="") as profitloss_csv:
     profitloss_reader=csv.reader(profitloss_csv,delimiter=",")
@@ -28,5 +26,5 @@ with open (budget_data,newline="") as profitloss_csv:
 #PRINT EM UP
 print("Financial Analysis")
 print("------------------")
-print(f"Total Months: {total}")
+print(f"Total Months: {value}")
 print(f"Total: $ {net}")
