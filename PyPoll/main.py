6bf474a588846cbce8fa5ffde32c12a523a89d7d
zovel <zoe.a.velie@gmail.com>
2018-09-05 17:40:45 -0500

    more and more code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,17 +3,31 @@ import os
 import csv
 election_data=os.path.join('election_data.csv')
 
-#The total number of votes cast
+   #The total number of votes cast
 with open (election_data,newline="") as totalcast_csv:
     totalcast_reader=csv.reader(totalcast_csv,delimiter=",")
     next(totalcast_reader,None)
     value=len(list(totalcast_reader))
 
+#Rewrite CSV 2 be clean 2 read 
+VoterID=[]
+Candidate=[]
+with open(election_data,newline="") as rewrite_csv:
+    rewrite_reader=csv.reader(rewrite_csv,delimiter=",")
+    for row in rewrite_reader:
+        VoterID.append(row[0])
+        Candidate.append(row[2])
+cleaned_csv=zip(VoterID,Candidate)
+output_file=os.path.join("clean_election_data")
+with open(output_file,"w",newline="") as datafile:
+    writer=csv.writer(datafile)
+    writer.writerows(cleaned_csv)
+
 #A complete list of candidates who received votes
-with open (election_data,newline="") as candidates_csv:
-    candidates_reader=csv.reader(candidates_csv,delimiter=",")
-    next(candidates_reader,None)
-    
+candidates_captured=csv.reader(cleaned_csv,delimiter=",")
+candidate_output=(row[1])
+for x in names:
+    if x not in candidate_output:
 
 #The percentage of votes each candidate won
 #with open (election_data,newline="") as percentagewon_csv:
