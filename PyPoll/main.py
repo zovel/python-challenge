5ecc5093d2801df9b87b84716581f6e802902be8
zovel <zoe.a.velie@gmail.com>
2018-09-06 22:16:40 -0500

    restructured

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,47 +1,57 @@
-#get em ready
 import os
 import csv
+voter_id=[]
+candidate=[]
+candidate_list=[]
+total_votes=0
+candidate_votes=[]
+winner=""
 election_data=os.path.join('election_data.csv')
+with open (election_data,newline="") as election_csv:
+    election_reader=csv.reader(election_csv,delimiter=",")
+    next(election_reader,None)
+    for row in election_reader:
+        total_votes +=1
+        voter_id.append(row[0])
+        candidate.append(row[2])
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+#number of candidates
+candidate_1=[name for name in candidate if name == candidate_list[0]]
+candidate_2=[name for name in candidate if name == candidate_list[1]]
+candidate_3=[name for name in candidate if name == candidate_list[2]]
+candidate_4=[name for name in candidate if name == candidate_list[3]]
 
-   #The total number of votes cast
-with open (election_data,newline="") as totalcast_csv:
-    totalcast_reader=csv.reader(totalcast_csv,delimiter=",")
-    next(totalcast_reader,None)
-    value=len(list(totalcast_reader))
+#candidate total/percentage
+candidate_1_total=len(candidate_1)
+candidate_votes.append(candidate_1_total)
+candidate_1_percent=float((candidate_1_total/total_votes)*100)
+candidate_2_total=len(candidate_2)
+candidate_votes.append(candidate_2_total)
+candidate_2_percent=float((candidate_2_total/total_votes)*100)
+candidate_3_total=len(candidate_3)
+candidate_votes.append(candidate_3_total)
+candidate_3_percent=float((candidate_3_total/total_votes)*100)
+candidate_4_total=len(candidate_4)
+candidate_votes.append(candidate_4_total)
+candidate_4_percent=float((candidate_4_total/total_votes)*100)
 
-#Rewrite CSV 2 be clean 2 read 
-VoterID=[]
-Candidate=[]
-with open(election_data,newline="") as rewrite_csv:
-    rewrite_reader=csv.reader(rewrite_csv,delimiter=",")
-    for row in rewrite_reader:
-        VoterID.append(row[0])
-        Candidate.append(row[2])
-cleaned_csv=zip(VoterID,Candidate)
-output_file=os.path.join("clean_election_data")
-with open(output_file,"w",newline="") as datafile:
-    writer=csv.writer(datafile)
-    writer.writerows(cleaned_csv)
+#find winner
+if max(candidate_votes)==candidate_1_total:
+    winner=candidate_list[0]
+elif max(candidate_votes)==candidate_2_total:
+    winner=candidate_list[1]
+elif max(candidate_votes)==candidate_3_total:
+    winner=candidate_list[2]
+else:
+    winner=candidate_list[3]
 
-#A complete list of candidates who received votes
-candidates_captured=csv.reader(cleaned_csv,delimiter=",")
-candidate_output=(row[1])
-for x in names:
-    if x not in candidate_output:
-
-#The percentage of votes each candidate won
-#with open (election_data,newline="") as percentagewon_csv:
-   #percentagewon_reader=csv.reader(percentagewon_csv,delimiter=",")
-   #next(percentagewon_reader,None)
-
-#The total number of votes each candidate won
-#with open (election_data,newline="") as totalwon_csv:
-    #totalwon_reader=csv.reader(totalwon_csv,delimiter=",")
-    #next(totalwon_reader,None)
-
-#The winner of the election based on popular vote.
 #print em up 
 print("Election Results")
 print("--------------------")
-print(f"Total Votes: {value}")
-
+print(f"Total Votes: {total_votes}")
+print(f"{candidate_list[0]}: {candidate_1_percent} % ({candidate_1_total})")
+print(f"{candidate_list[1]}: {candidate_2_percent} % ({candidate_2_total})")
+print(f"{candidate_list[2]}: {candidate_3_percent} % ({candidate_3_total})")
+print(f"{candidate_list[3]}: {candidate_4_percent} % ({candidate_4_total})")
+print(f"Winner: {winner}")
