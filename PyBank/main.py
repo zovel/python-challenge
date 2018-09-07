b4ea0a341bb9808e1cdb65df19cd1bdb1986054b
zovel <zoe.a.velie@gmail.com>
2018-09-06 21:36:42 -0500

    tabulated code post error message

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,60 +1,41 @@
-#get em ready
 import os
 import csv
-budget_data=os.path.join('budget_data.csv')
 net=0
-profloss=[]
-change=
-
-#The total number of months included in the dataset
-with open(budget_data,newline="") as totalmonths_csv:
-    totalmonths_reader=csv.reader(totalmonths_csv,delimiter=",")
-    next(totalmonths_reader,None)
-    value=len(list(totalmonths_reader))
-
-#The total net amount of "Profit/Losses" over the entire period
-for row in totalmonths_reader:
-    net +=int(row[1])
-
-#The average change in "Profit/Losses" between months over the entire period
-with open (budget_data,newline="") as averagechange_csv:
-    averagechange_reader=csv.reader(averagechange_csv,delimiter=",")
-    next(averagechange_reader,None)
-
-for row in averagechange_reader:
-    def change_in_profloss(profloss):
-       return(int(row[1]-change))
-       change = change_in_profloss(profloss)
-average_change = (sum(profloss)/int(len(profloss)))
-
-
-
-#The greatest increase in profits (date and amount) over the entire period
-with open (budget_data,newline="") as greatestincrease_csv:
-    greatestincrease_reader=csv.reader(greatestincrease_csv,delimiter=",")
-    next(greatestincrease_reader,None)
-  
-greatest_increase = max([value for value in profloss])
-greatest_decrease = min([value for value in profloss])
-   for row in monthlyanalysis:
-       if row[1]= greatest_increase:
-           month1 = (row[0])
-       if row[1]= greatest_decrease:
-           month2 = (row[0])
-
-
-#The greatest decrease in losses (date and amount) over the entire period
-with open (budget_data,newline="") as greatestdecrease_csv:
-    greatestdecrease_reader=csv.reader(greatestdecrease_csv,delimiter=",")
-    next(greatestdecrease_reader,None)
-    #use enumerate function here
-
+profit_loss=[]
+total_months=0
+greatest_increase, greatest_decrease=0,0
+difference=0
+month_1=""
+month_2=""
+budget_data=os.path.join("budget_data.csv")
+with open(budget_data,newline="") as budget_csv:
+    budget_reader=csv.reader(budget_csv,delimiter=",")
+    next(budget_reader,None)
+    total_months +=1
+    first_row= next(budget_reader)
+    change=int(first_row[1])
+    net=int(first_row[1])
+    def change_in_profit_loss(change):
+        return((int(row[1]))-change)
+    for row in budget_reader:
+        total_months +=1
+        net += int(row[1])
+        difference= change_in_profit_loss(change)
+        change=int(row[1])
+        profit_loss.append(difference)
+        if difference >= greatest_increase:
+            greatest_increase= difference
+            month_1= (row[0])
+        elif difference <= greatest_decrease:
+            greatest_decrease= difference
+            month_2=(row[0])
+    average_change= (sum(profit_loss)/int(len(profit_loss)))
 
 #PRINT EM UP
 print("Financial Analysis")
 print("------------------")
-print(f"Total Months: {value}")
+print(f"Total Months: {total_months}")
 print(f"Total: $ {net}")
-#print(f"Average Change:)
-#print("Greatest Increase in Profits")
-#print("Greatest Decrease in Profits")
+print(f"Average Change: $ {average_change}")
+print(f"Greatest Increase: {month_1} $ {greatest_increase}")
+print(f"Greatest Decrease: {month_2} $ {greatest_decrease}")
